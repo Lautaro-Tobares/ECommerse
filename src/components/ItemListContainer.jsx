bf9496bcfbb7375e02b8a8565a598dc6f9d9ef2f
Lautaro tobares <lautarotobares282@gmail.com>
2024-03-31 18:31:10 -0300

    asas

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,9 @@
 import Container from 'react-bootstrap/Container';
 
+import data from '../data/products.json';
+
+console.log(data);
+
 export const ItemListContainer = (props) => {
 
     return <Container className='mt-4'> <div>{props.greetings} </div> </Container>;
