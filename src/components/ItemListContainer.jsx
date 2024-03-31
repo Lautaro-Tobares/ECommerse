e811e459413e7698562011bda3172d5dd248e6a9
Lautaro tobares <lautarotobares282@gmail.com>
2024-03-31 19:13:54 -0300

    add

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 
 import data from '../data/products.json';
@@ -6,5 +8,20 @@ console.log(data);
 
 export const ItemListContainer = (props) => {
 
-    return <Container className='mt-4'> <div>{props.greetings} </div> </Container>;
+    const [Items, setItems]= useState ([]);
+
+    useEffect (()=>{
+        const get = new Promise((resolve, reject) => {
+            resolve (data)
+        });
+
+    },[]);
+
+    return 
+    <Container className='mt-4'> 
+    
+    <div>{props.greetings} </div> 
+
+    </Container>;
+    
 };
